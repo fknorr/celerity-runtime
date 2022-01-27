e2d23d3de4a8c31fe2f94350ede20337b97b4b26
Fabian Knorr <fabian.knorr@uibk.ac.at>
2022-01-27 12:09:16 +0100

    Output dependency context on graph printing; drop ORDER dependency kind
    ORDER_DEP was handled like TRUE_DEP everywhere, and it does not add any value except for context information in printed graphs.
    This adds a dependency_origin enum to distinguish data flow dependencies from epoch/horizon/collective temporal dependencies.

--- test/unit_test_suite_celerity.h
+++ test/unit_test_suite_celerity.h
@@ -9,5 +9,5 @@ class task_manager;
 } // namespace celerity::detail
 
 void maybe_print_graph(celerity::detail::task_manager& tm);
-void maybe_print_graph(celerity::detail::command_graph& cdag);
+void maybe_print_graph(celerity::detail::command_graph& cdag, const celerity::detail::task_manager &tm);
 void maybe_print_graphs(celerity::test_utils::cdag_test_context& ctx);
