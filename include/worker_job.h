b0a583d73e8cf0937245741b3efca18f24956b00
Fabian Knorr <fabian.knorr@uibk.ac.at>
2022-02-02 09:50:07 +0100

    Clean up and document epochs implementation

--- include/worker_job.h
+++ include/worker_job.h
@@ -37,7 +37,9 @@ namespace detail {
 
 	  protected:
 		template <typename... Es>
-		worker_job(command_pkg pkg, std::tuple<Es...> ctx = {}) : pkg(pkg), lctx(std::tuple_cat(std::make_tuple("job", pkg.cid), ctx)) {}
+		explicit worker_job(command_pkg pkg, std::tuple<Es...> ctx = {})
+		    : pkg(pkg), lctx(pkg.tid.has_value() ? log_context{std::tuple_cat(std::tuple{"task", *pkg.tid, "job", pkg.cid}, ctx)}
+		                                         : log_context{std::tuple_cat(std::tuple{"job", pkg.cid}, ctx)}) {}
 
 	  private:
 		command_pkg pkg;
@@ -62,9 +64,7 @@ namespace detail {
 
 	class horizon_job : public worker_job {
 	  public:
-		horizon_job(command_pkg pkg, task_manager& tm) : worker_job(pkg, std::make_tuple("tid", std::get<horizon_data>(pkg.data).tid)), task_mngr(tm) {
-			assert(pkg.cmd == command_type::HORIZON);
-		}
+		horizon_job(command_pkg pkg, task_manager& tm) : worker_job(pkg), task_mngr(tm) { assert(pkg.cmd == command_type::HORIZON); }
 
 	  private:
 		task_manager& task_mngr;
@@ -75,7 +75,7 @@ namespace detail {
 
 	class epoch_job : public worker_job {
 	  public:
-		epoch_job(command_pkg pkg, std::shared_ptr<logger> job_logger, task_manager& tm) : worker_job(pkg, job_logger), task_mngr(tm) {
+		epoch_job(command_pkg pkg, task_manager& tm) : worker_job(pkg), task_mngr(tm), action(std::get<epoch_data>(pkg.data).action) {
 			assert(pkg.cmd == command_type::EPOCH);
 		}
 
@@ -85,8 +85,8 @@ namespace detail {
 		task_manager& task_mngr;
 		epoch_action action;
 
-		bool execute(const command_pkg& pkg, std::shared_ptr<logger> logger) override;
-		std::pair<command_type, std::string> get_description(const command_pkg& pkg) override;
+		bool execute(const command_pkg& pkg) override;
+		std::string get_description(const command_pkg& pkg) override;
 	};
 
 	/**
@@ -121,7 +121,7 @@ namespace detail {
 
 	class reduction_job : public worker_job {
 	  public:
-		reduction_job(command_pkg pkg, reduction_manager& rm) : worker_job(pkg, std::make_tuple("rid", std::get<reduction_data>(pkg.data).rid)), rm(rm) {
+		reduction_job(command_pkg pkg, reduction_manager& rm) : worker_job(pkg, std::tuple{"rid", std::get<reduction_data>(pkg.data).rid}), rm(rm) {
 			assert(pkg.cmd == command_type::REDUCTION);
 		}
 
@@ -136,7 +136,7 @@ namespace detail {
 	class host_execute_job : public worker_job {
 	  public:
 		host_execute_job(command_pkg pkg, detail::host_queue& queue, detail::task_manager& tm, buffer_manager& bm)
-		    : worker_job(pkg, std::make_tuple("tid", std::get<execution_data>(pkg.data).tid)), queue(queue), task_mngr(tm), buffer_mngr(bm) {
+		    : worker_job(pkg), queue(queue), task_mngr(tm), buffer_mngr(bm) {
 			assert(pkg.cmd == command_type::EXECUTION);
 		}
 
@@ -158,8 +158,7 @@ namespace detail {
 	class device_execute_job : public worker_job {
 	  public:
 		device_execute_job(command_pkg pkg, detail::device_queue& queue, detail::task_manager& tm, buffer_manager& bm, reduction_manager& rm, node_id local_nid)
-		    : worker_job(pkg, std::make_tuple("tid", std::get<execution_data>(pkg.data).tid)), queue(queue), task_mngr(tm), buffer_mngr(bm), reduction_mngr(rm),
-		      local_nid(local_nid) {
+		    : worker_job(pkg), queue(queue), task_mngr(tm), buffer_mngr(bm), reduction_mngr(rm), local_nid(local_nid) {
 			assert(pkg.cmd == command_type::EXECUTION);
 		}
 
