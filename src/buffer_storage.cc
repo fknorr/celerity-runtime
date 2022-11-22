28ad847d2717bb4ce68ccabab8edaffbb10c094b
Philip Salzmann <philip.salzmann@uibk.ac.at>
2022-11-22 19:31:32 +0100

    [USM][SM] Use cudaMemcpy for 1D copies as well (instead of SYCL copy)
    Also fix some unused variable warnings when compiling with hipSYCL

--- src/buffer_storage.cc
+++ src/buffer_storage.cc
@@ -47,17 +47,24 @@ namespace detail {
 	    const cl::sycl::range<1>& source_range, const cl::sycl::id<1>& source_offset, const cl::sycl::range<1>& target_range,
 	    const cl::sycl::id<1>& target_offset, const cl::sycl::range<1>& copy_range) {
 		const size_t line_size = elem_size * copy_range[0];
+#if defined(__HIPSYCL__)
+		const auto ret = cudaMemcpy(reinterpret_cast<char*>(target_base_ptr) + elem_size * get_linear_index(target_range, target_offset),
+		    reinterpret_cast<const char*>(source_base_ptr) + elem_size * get_linear_index(source_range, source_offset), line_size, cudaMemcpyDefault);
+		if(ret != cudaSuccess) throw std::runtime_error("cudaMemcpy2D failed");
+		// Classic CUDA footgun: Memcpy is not always synchronous (e.g. for D2D)
+		cudaStreamSynchronize(0);
+#else
 		queue
 		    .memcpy(reinterpret_cast<char*>(target_base_ptr) + elem_size * get_linear_index(target_range, target_offset),
 		        reinterpret_cast<const char*>(source_base_ptr) + elem_size * get_linear_index(source_range, source_offset), line_size)
 		    .wait();
+#endif
 	}
 
 	// TODO Optimize for contiguous copies?
 	void memcpy_strided_device(cl::sycl::queue& queue, const void* source_base_ptr, void* target_base_ptr, size_t elem_size,
 	    const cl::sycl::range<2>& source_range, const cl::sycl::id<2>& source_offset, const cl::sycl::range<2>& target_range,
 	    const cl::sycl::id<2>& target_offset, const cl::sycl::range<2>& copy_range) {
-		const size_t line_size = elem_size * copy_range[1];
 		const auto source_base_offset = get_linear_index(source_range, source_offset);
 		const auto target_base_offset = get_linear_index(target_range, target_offset);
 
@@ -70,6 +77,7 @@ namespace detail {
 		// Classic CUDA footgun: Memcpy is not always synchronous (e.g. for D2D)
 		cudaStreamSynchronize(0);
 #else
+		const size_t line_size = elem_size * copy_range[1];
 		std::vector<cl::sycl::event> wait_list;
 		wait_list.reserve(copy_range[0]);
 		for(size_t i = 0; i < copy_range[0]; ++i) {
@@ -87,16 +95,10 @@ namespace detail {
 	void memcpy_strided_device(cl::sycl::queue& queue, const void* source_base_ptr, void* target_base_ptr, size_t elem_size,
 	    const cl::sycl::range<3>& source_range, const cl::sycl::id<3>& source_offset, const cl::sycl::range<3>& target_range,
 	    const cl::sycl::id<3>& target_offset, const cl::sycl::range<3>& copy_range) {
-		// We simply decompose this into a bunch of 2D copies. Subtract offset on the copy plane, as it will be added again during the 2D copy.
-		const auto source_base_offset =
-		    get_linear_index(source_range, source_offset) - get_linear_index({source_range[1], source_range[2]}, {source_offset[1], source_offset[2]});
-		const auto target_base_offset =
-		    get_linear_index(target_range, target_offset) - get_linear_index({target_range[1], target_range[2]}, {target_offset[1], target_offset[2]});
-
 // NOCOMMIT Move into backend-specific module
 #if defined(__HIPSYCL__)
 		// NOCOMMIT TODO This needs thorough testing. I don't think current unit tests exercise strided 3D copies much (if at all)
-		cudaMemcpy3DParms parms = {0};
+		cudaMemcpy3DParms parms = {};
 		parms.srcPos = make_cudaPos(source_offset[2] * elem_size, source_offset[1], source_offset[0]);
 		parms.srcPtr = make_cudaPitchedPtr(const_cast<void*>(source_base_ptr), source_range[2] * elem_size, source_range[2], source_range[1]);
 		parms.dstPos = make_cudaPos(target_offset[2] * elem_size, target_offset[1], target_offset[0]);
@@ -108,6 +110,12 @@ namespace detail {
 		// Classic CUDA footgun: Memcpy is not always synchronous (e.g. for D2D)
 		cudaStreamSynchronize(0);
 #else
+		// We simply decompose this into a bunch of 2D copies. Subtract offset on the copy plane, as it will be added again during the 2D copy.
+		const auto source_base_offset =
+		    get_linear_index(source_range, source_offset) - get_linear_index({source_range[1], source_range[2]}, {source_offset[1], source_offset[2]});
+		const auto target_base_offset =
+		    get_linear_index(target_range, target_offset) - get_linear_index({target_range[1], target_range[2]}, {target_offset[1], target_offset[2]});
+
 		for(size_t i = 0; i < copy_range[0]; ++i) {
 			const auto source_ptr = reinterpret_cast<const char*>(source_base_ptr) + elem_size * (source_base_offset + i * (source_range[1] * source_range[2]));
 			const auto target_ptr = reinterpret_cast<char*>(target_base_ptr) + elem_size * (target_base_offset + i * (target_range[1] * target_range[2]));
