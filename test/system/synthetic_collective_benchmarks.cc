9704b6b7512989d3a60111cd83cb1a1f0ab67f9a
Fabian Knorr <git@fabian-knorr.info>
2023-02-11 08:30:28 +0100

    Alltoall synthetic_collective_benchmark

--- test/system/synthetic_collective_benchmarks.cc
+++ test/system/synthetic_collective_benchmarks.cc
@@ -16,9 +16,10 @@
 using namespace celerity;
 using namespace celerity::test_utils;
 
-unsigned long long operator""_Ki(unsigned long long v) { return v << 10; }
-unsigned long long operator""_Mi(unsigned long long v) { return v << 20; }
-unsigned long long operator""_Gi(unsigned long long v) { return v << 30; }
+size_t operator""_i(unsigned long long v) { return v; }
+size_t operator""_Ki(unsigned long long v) { return v << 10; }
+size_t operator""_Mi(unsigned long long v) { return v << 20; }
+size_t operator""_Gi(unsigned long long v) { return v << 30; }
 
 template <typename F>
 std::chrono::microseconds run_distributed_benchmark(size_t n_warmup, size_t n_passes, F&& f) {
@@ -43,11 +44,14 @@ bool is_master_node() {
 	return rank == 0;
 }
 
+// TODO this turns out to be really slow on Marconi with collectives enabled for 2 and 4 nodes.
+//	- run again with device broadcast enabled
+//  - is there a way to get_buffer_data() from multiple devices without the host-buffer hop?
 TEST_CASE_METHOD(runtime_fixture, "Allgather") {
 	const size_t n_warmup = 2;
 	const size_t n_passes = 20;
 	const size_t n_iters = 100;
-	const size_t range = GENERATE(values({2_Ki, 16_Ki, 256_Ki, 2_Mi, 16_Mi /*, 256_Mi, 2_Gi */}));
+	const size_t range = GENERATE(values({4_Ki, 256_Ki, 16_Mi, 256_Mi}));
 
 	celerity::buffer<int> buf(range);
 
@@ -74,36 +78,69 @@ TEST_CASE_METHOD(runtime_fixture, "Allgather") {
 	if(is_master_node()) { fmt::print("Allgather;{};{}\n", range, time.count()); }
 }
 
-TEST_CASE_METHOD(runtime_fixture, "Gather-Bcast") {
+TEST_CASE_METHOD(runtime_fixture, "Gather-Scatter") {
 	const size_t n_warmup = 2;
 	const size_t n_passes = 20;
 	const size_t n_iters = 100;
-	const size_t range = GENERATE(values({2_Ki, 16_Ki, 256_Ki, 2_Mi, 16_Mi /*, 256_Mi, 2_Gi */}));
+	const size_t range = GENERATE(values({4_Ki, 256_Ki, 16_Mi, 256_Mi}));
 
-	celerity::buffer<int> buf_1(range);
-	celerity::buffer<int> buf_2(1);
+	celerity::buffer<int> buf(range);
 
 	const auto time = run_distributed_benchmark(n_warmup, n_passes, [&](celerity::distr_queue& q) mutable {
 		q.submit([=](celerity::handler& cgh) {
-			accessor read_acc(buf_1, cgh, access::all(), read_only);
-			accessor write_acc(buf_2, cgh, access::one_to_one(), write_only, no_init);
-			cgh.parallel_for<class UKN(allgather)>(celerity::range<1>(1), [=](celerity::item<1>) {
-				(void)read_acc;
+			accessor write_acc(buf, cgh, access::one_to_one(), write_only, no_init);
+			cgh.parallel_for<class UKN(init)>(celerity::range<1>(range), [=](celerity::item<1> it) { //
 				(void)write_acc;
 			});
 		});
 
 		for(size_t i = 0; i < n_iters; ++i) {
 			q.submit([=](celerity::handler& cgh) {
-				accessor read_acc(buf_2, cgh, access::all(), read_only);
-				accessor write_acc(buf_1, cgh, access::one_to_one(), write_only, no_init);
-				cgh.parallel_for<class UKN(allgather)>(celerity::range<1>(range), [=](celerity::item<1>) {
-					(void)read_acc;
-					(void)write_acc;
-				});
+				accessor acc(buf, cgh, access::all(), read_write);
+				cgh.parallel_for<class UKN(gather)>(celerity::range<1>(1), [=](celerity::item<1>) { (void)acc; });
+			});
+
+			q.submit([=](celerity::handler& cgh) {
+				accessor acc(buf, cgh, access::all(), read_write);
+				cgh.parallel_for<class UKN(scatter)>(celerity::range<1>(range), [=](celerity::item<1>) { (void)acc; });
 			});
 		}
 	});
 
 	if(is_master_node()) { fmt::print("Gather-Bcast;{};{}\n", range, time.count()); }
+}
+
+TEST_CASE_METHOD(runtime_fixture, "Alltoall") {
+	const size_t n_warmup = 2;
+	const size_t n_passes = 20;
+	const size_t n_iters = 100;
+	const size_t size = GENERATE(values({64_i, 512_i, 4_Ki, 16_Ki}));
+	const range<2> range(size, size);
+
+	celerity::buffer<int, 2> buf_a(range);
+	celerity::buffer<int, 2> buf_b(range);
+
+	const auto time = run_distributed_benchmark(n_warmup, n_passes, [&](celerity::distr_queue& q) mutable {
+		q.submit([=](celerity::handler& cgh) {
+			accessor write_acc(buf_a, cgh, access::one_to_one(), write_only, no_init);
+			cgh.parallel_for<class UKN(init)>(range, [=](celerity::item<2> it) { //
+				(void)write_acc;
+			});
+		});
+
+		for(size_t i = 0; i < n_iters; ++i) {
+			q.submit([=](celerity::handler& cgh) {
+				accessor read_acc(buf_a, cgh, experimental::access::transposed<1, 0>(), read_only);
+				accessor write_acc(buf_b, cgh, access::one_to_one(), write_only, no_init);
+				cgh.parallel_for<class UKN(gather)>(range, [=](celerity::item<2>) {
+					(void)read_acc;
+					(void)write_acc;
+				});
+			});
+
+			std::swap(buf_a, buf_b);
+		}
+	});
+
+	if(is_master_node()) { fmt::print("Allgather;{};{}\n", range.size(), time.count()); }
 }
